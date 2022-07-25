517fd23a1b46866176b00862f58307c57b03a5e6
ZelalemGG <midzele@gmail.com>
2022-07-25 04:22:53 +0300

    Cleaning auto generated contract

--- contracts/GoFundMe.sol
+++ contracts/GoFundMe.sol
@@ -5,9 +5,12 @@ pragma solidity ^0.8.9;
 @title Go Fund Me (with Best practice)
 @license GNU GPLv3
 @author Zelalem Gebrekirstos
-@notice A crowd funding contract to create funding campaigns with specific goal
-    and able to withdraw when closed. 
-    This project demonestrates best practices to achive high security and low gas fees.
+@notice A crowd funding contract to create funding campaigns with specific goal 
+(target fund) and able to withdraw when the campaign is closed. 
+This project demonestrates best practices to achive high security and low gas fees.
+
+@Notice You may read a detailed explanation of my blog at http://zillo.one/blog-go-fund-me
+or reach me out via info@zillo.one
 */
 
 import "hardhat/console.sol";
@@ -22,62 +25,79 @@ contract GoFundMe is PullPayment {
     uint targetFund;
     bool campainIsActive;
     uint totalFund;
-    
+
     address[] donators;
-    mapping (address => uint) donations;
+    mapping(address => uint) donations;
 
-    // using EnumerableMap for EnumerableMap.AddressToUintMap;
-    // EnumerableMap.AddressToUintMap public donators;
+    /*  you can also alteratively use enumerable maps from openzeppelin 
+        to manage donators such as get No. of donators...like below
 
-    function init(string memory _campaignName, uint _minDonation, uint _targetFund) external {
+        using EnumerableMap for EnumerableMap.AddressToUintMap;
+        EnumerableMap.AddressToUintMap public donators;
+    */
+
+    function init(
+        string memory _campaignName,
+        uint _minDonation,
+        uint _targetFund
+    ) external {
         owner = msg.sender;
         campainName = _campaignName;
         minimumDonation = _minDonation;
         targetFund = _targetFund;
     }
 
-    modifier onlyOwner {
-        if(msg.sender == owner){
+    modifier onlyOwner() {
+        if (msg.sender == owner) {
             _;
         }
     }
 
     function makeDonation() external payable {
         uint _amount = msg.value;
-        require(campainIsActive, "Campaign not currently active");        
-        require(_amount >= minimumDonation, string(abi.encodePacked("Minimum donation is ", minimumDonation)));
+        require(campainIsActive, "Campaign not currently active");
+        require(
+            _amount >= minimumDonation,
+            string(abi.encodePacked("Minimum donation is ", minimumDonation))
+        );
         donations[msg.sender] = _amount;
-        donators.push(msg.sender);        
+        totalFund += _amount;
+        donators.push(msg.sender);
 
         //close campain if target is reached\\
-        if(address(this).balance >= targetFund){
-            campainIsActive = false;            
+        if (address(this).balance >= targetFund) {
+            campainIsActive = false;
         }
     }
 
-    function withdraw () public onlyOwner {
+    function withdraw() public onlyOwner {
         require(!campainIsActive, "Campaign not closed yet");
-        
+        require(
+            address(this).balance > minimumDonation,
+            "insufficient balance"
+        );
+
         //using the asyncTransfer func from openzeppelin PullPayment
         //contract is safer as its stored in an intermediate Escrow contract
         _asyncTransfer(owner, totalFund);
         console.log("Funds delivered to owner. Amount = %s", totalFund);
         console.log("Funds delivered to owner. Amount = %s", totalFund);
-
     }
 
-    //TODO: Just addred to experiment security bypass
-    // remove it once done... :)
-    function tester(string memory name) public view  {
-        
-        console.log("Executed...", name);
-    }
-
-    function closeCampaign () external onlyOwner (){
+    function closeCampaign() external onlyOwner {
         campainIsActive = false;
         console.log("Campaign cancelled by owner!");
     }
 
-
-
+    function checkCampaignStatus()
+        public
+        view
+        onlyOwner
+        returns (string memory _status)
+    {
+        if (campainIsActive) {
+            _status = "Active";
+        }
+        return _status = "Closed";
+    }
 }
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -22,14 +22,14 @@ async function main() {
 
   console.log("Account balance:", (await deployer.getBalance()).toString());
 
-  const Token = await ethers.getContractFactory("Token");
-  const token = await Token.deploy();
-  await token.deployed();
+  const factory = await ethers.getContractFactory("Factory");
+  const campaignFactory = await factory.deploy();
+  await campaignFactory.deployed();
 
-  console.log("Token address:", token.address);
+  console.log("Token address:", campaignFactory.address);
 
   // We also save the contract's artifacts and address in the frontend directory
-  saveFrontendFiles(token);
+  saveFrontendFiles(campaignFactory);
 }
 
 function saveFrontendFiles(token) {
